dedb137cd7521ccd31cf270768b4ac1e88d95590
Andreas Johansson <glanshammar70@gmail.com>
2024-11-16 21:28:45 +0100

    Updated global usings

--- Gymbokning/GlobalUsings.cs
+++ Gymbokning/GlobalUsings.cs
@@ -1,4 +1,8 @@
-﻿global using Microsoft.AspNetCore.Identity;
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Identity;
 global using Gymbokning.Data;
 global using Gymbokning.Models;
 global using Microsoft.EntityFrameworkCore;
