16a7191103ca0345dd5cb466f0be3d26a7dfbf1c
Andreas Johansson <glanshammar70@gmail.com>
2024-11-16 21:13:04 +0100

    Added a minimum time ValidationAttribute

---
+++ Gymbokning/Models/Attributes/MinimumClassTime.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gymbokning.Models.Attributes
+{
+    public class MinimumClassTime : ValidationAttribute
+    {
+        private readonly TimeSpan _minValue;
+
+        public MinimumClassTime(int hours, int minutes)
+        {
+            _minValue = new TimeSpan(hours, minutes, 0); // Create a TimeSpan from hours and minutes
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is TimeSpan timeSpanValue)
+            {
+                if (timeSpanValue < _minValue)
+                {
+                    return new ValidationResult($"The duration must be at least {_minValue.Hours} hours and {_minValue.Minutes} minutes.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- Gymbokning/Models/GymClass.cs
+++ Gymbokning/Models/GymClass.cs
@@ -1,12 +1,20 @@
-﻿namespace Gymbokning.Models
+﻿using Gymbokning.Models.Attributes;
+
+namespace Gymbokning.Models
 {
     public class GymClass
     {
         public int Id { get; set; }
+        
         public string Name { get; set; }
+        
         public DateTime StartTime { get; set; }
+
+        [MinimumClassTime(0, 1)]
         public TimeSpan Duration { get; set; }
+        
         public DateTime EndTime { get { return StartTime + Duration; } }
+        
         public string Description { get; set; }
 
         // Navigation property for attending members
