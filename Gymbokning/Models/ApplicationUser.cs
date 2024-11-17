069b922e22567d8cf68ad42ebc8cfa6e02a15018
Andreas Johansson <glanshammar70@gmail.com>
2024-11-17 20:50:40 +0100

    Updated ApplicationUser model

--- Gymbokning/Models/ApplicationUser.cs
+++ Gymbokning/Models/ApplicationUser.cs
@@ -2,6 +2,10 @@
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime TimeOfRegistration { get; set; }
         public ICollection<ApplicationUserGymClass> AttendedClasses { get; set; } = new List<ApplicationUserGymClass>();
     }
 }
