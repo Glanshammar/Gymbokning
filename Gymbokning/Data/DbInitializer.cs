266ddb8a1f838f7dba32419c31eccf1b6e99ff92
Andreas Johansson <glanshammar70@gmail.com>
2024-11-17 17:26:02 +0100

    Added database initializer

---
+++ Gymbokning/Data/DbInitializer.cs
@@ -0,0 +1,49 @@
+namespace Gymbokning.Data;
+
+public class DbInitializer
+{
+    public static async Task Initialize(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            await SeedRoles(roleManager);
+            await SeedUsers(userManager);
+        }
+    }
+    
+    public static async Task SeedData(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        await SeedRoles(roleManager);
+        await SeedUsers(userManager);
+    }
+
+    private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+    {
+        if (!await roleManager.RoleExistsAsync("Admin"))
+        {
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+        }
+    }
+
+    private static async Task SeedUsers(UserManager<ApplicationUser> userManager)
+    {
+        if (await userManager.FindByEmailAsync("admin@gymbokning.se") == null)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = "admin@gymbokning.se",
+                Email = "admin@gymbokning.se",
+                EmailConfirmed = true
+            };
+            var result = await userManager.CreateAsync(user, "Admin123!");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+    }
+}
